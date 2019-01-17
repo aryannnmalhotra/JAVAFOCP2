8bb077795e7d70e96a8b035ec64c0b7474465e31
Amalhotra29 <apoorva14csu034@ncuindia.edu>
2019-01-17 11:17:55 +0530

    initial push

--- classes2/program/src/point.java
+++ classes2/program/src/point.java
@@ -1,6 +1,6 @@
 class Point{
-	private double x;
-	private double y;
+	static double x;
+	static double y;
 	
 	Point()
 {
@@ -15,19 +15,19 @@ Point(double x){
 Point(double x, double y){this.x = x;
 this.y = y;}
 
-double getX(){
+static double getX(){
 	return x;
 }
-double getY(){
+static double getY(){
 	return y;
 }
-void setX(double x){
+static void setX(double x){
 	this.x = x;
 }
-void setY(double y){
+static void setY(double y){
 	this.y = y;
 }
-Point distance(Point p1){
+static Point distance(Point p1){
 	Point p2 = new Point();
 		p2.x = p1.x = this.x;
 		p2.y = p1.y = this.y;
